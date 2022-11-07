f8a8ac258e66d6436448b3ac1a47d0cc6e02c798
Jeremy Dufeux <jeremy.dufeux@gmail.com>
2022-11-07 11:14:34 +0100

    Update Gefen_matrix_controller.ino
    get button state

--- Gefen_matrix_controller.ino
+++ Gefen_matrix_controller.ino
@@ -1,56 +1,85 @@
-const int pinBtn1 =  0;
-const int pinBtn2 =  1;
-const int pinBtn3 =  2;
-const int pinBtn4 =  3;
+const int pinBtn1 =  2;
+const int pinBtn2 =  3;
+const int pinBtn3 =  4;
+const int pinBtn4 =  5;
 const int pinLedBtn1 =  10;
 const int pinLedBtn2 =  11;
 const int pinLedBtn3 =  12;
+const int pinLedBtn4 =  13;
+
+int button1State = HIGH;
+int button2State = HIGH;
+int button3State = HIGH;
+int button4State = HIGH;
+int lastButton1State = HIGH;
+int lastButton2State = HIGH;
+int lastButton3State = HIGH;
+int lastButton4State = HIGH;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(pinBtn1, INPUT_PULLUP);
   pinMode(pinBtn2, INPUT_PULLUP);
   pinMode(pinBtn3, INPUT_PULLUP);
   pinMode(pinBtn4, INPUT_PULLUP);
-  
   pinMode(pinLedBtn1, OUTPUT);
   pinMode(pinLedBtn2, OUTPUT);
   pinMode(pinLedBtn3, OUTPUT);
   pinMode(pinLedBtn4, OUTPUT);
 }
 
-int button1 = 0;    
-int button2 = 0;    
-int button3 = 0;    
-int button4 = 0;    
-
-// the loop function runs over and over again forever
 void loop() {
-  button1 = digitalRead(pinBtn1);
-  button2 = digitalRead(pinBtn2);
-  button3 = digitalRead(pinBtn3);
-  button4 = digitalRead(pinBtn4);
-  
-  if (button1 == HIGH) {
-    digitalWrite(pinLedBtn1, LOW);
-  } else {
-    digitalWrite(pinLedBtn1, HIGH);
+  checkButtonsStates();
+}
+
+void checkButtonsStates(){
+  button1State = digitalRead(pinBtn1);
+  button2State = digitalRead(pinBtn2);
+  button3State = digitalRead(pinBtn3);
+  button4State = digitalRead(pinBtn4);
+
+  if (button1State != lastButton1State) {
+    if (button1State == HIGH) {
+      Serial.println("1 off");
+    } else {
+      Serial.println("1 on");
+    }
+    delay(50);
+  }
+
+  if (button2State != lastButton2State) {
+    if (button2State == HIGH) {
+      Serial.println("2 off");
+    } else {
+      Serial.println("2 on");
+    }
+    delay(50);
+  }
+
+  if (button3State != lastButton3State) {
+    if (button3State == HIGH) {
+      Serial.println("3 off");
+    } else {
+      Serial.println("3 on");
+    }
+    delay(50);
+  }
+
+  if (button4State != lastButton4State) {
+    if (button4State == HIGH) {
+      Serial.println("4 off");
+    } else {
+      Serial.println("4 on");
+    }
+    delay(50);
   }
   
-  if (button2 == HIGH) {
-    digitalWrite(pinLedBtn2, LOW);
-  } else {
-    digitalWrite(pinLedBtn2, HIGH);
-  }
+  lastButton1State = button1State;
+  lastButton2State = button2State;
+  lastButton3State = button3State;
+  lastButton4State = button4State;
+}
+
+void sendMatrixPreset(){
   
-  if (button3 == HIGH) {
-    digitalWrite(pinLedBtn3, LOW);
-  } else {
-    digitalWrite(pinLedBtn3, HIGH);
-  }
-  
-  if (button4 == HIGH) {
-    digitalWrite(pinLedBtn4, LOW);
-  } else {
-    digitalWrite(pinLedBtn4, HIGH);
-  }
 }
